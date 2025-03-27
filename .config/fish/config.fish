71f7c695f15a1c2815b59f8fbbfaa7b73e7e0d9a
e-altunel <demirhan.altunel+github@gmail.com>
2025-03-27 18:26:24 +0300

    Add Fish shell configuration and custom prompt functions

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias ls='ls --color=auto'
+alias ll='ls -l --color=auto'
+alias la='ls -a --color=auto'
+alias lla='ls -la --color=auto'
+alias l='ls -CF --color=auto'
+alias ..='cd ..'
+alias v='nvim'
+alias vi='nvim'
+alias vim='nvim'
+alias grep='grep --color=auto'
+alias c='code'
+alias cls='clear'
+alias copy='xclip -selection clipboard'
